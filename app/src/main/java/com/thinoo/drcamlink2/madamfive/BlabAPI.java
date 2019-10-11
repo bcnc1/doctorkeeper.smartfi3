27a353d7787ce8451f3eafd3317dd06f905b8302
kimcy <kim.cy@outlook.com>
2019-10-11 18:40:54 +0900

    향후 사용할 api들

---
+++ app/src/main/java/com/thinoo/drcamlink2/madamfive/BlabAPI.java
@@ -0,0 +1,9 @@
+package com.thinoo.drcamlink2.madamfive;
+
+public class BlabAPI {
+    private static final String TAG = BlabAPI.class.getSimpleName();
+    private static final String BASE_URL = "https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_10b1107b-ce24-4cb4-a066-f46c53b474a3'";
+
+    private static String mAcccessToken = null;
+
+}
