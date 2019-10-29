5ff3013f8686f421e01e103bbcbbf27adb48c048
kimcy <kim.cy@outlook.com>
2019-10-29 16:33:45 +0900

    문제생길까봐

--- app/src/main/java/com/thinoo/drcamlink2/activities/LaunchVrecordActivity.java
+++ app/src/main/java/com/thinoo/drcamlink2/activities/LaunchVrecordActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.thinoo.drcamlink2.R;
+
 public class LaunchVrecordActivity extends Activity {
     private final String TAG = LaunchVrecordActivity.class.getSimpleName();
 
@@ -12,5 +14,7 @@ public class LaunchVrecordActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.launch_camera_activity_main);
     }
 }
