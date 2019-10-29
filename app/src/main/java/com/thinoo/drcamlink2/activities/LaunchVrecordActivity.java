202518fd7b93aa23d02f07090eec9c69b57cd2f4
kimcy <kim.cy@outlook.com>
2019-10-29 16:24:32 +0900

    비디오 액티비티

--- app/src/main/java/com/thinoo/drcamlink2/activities/LaunchVrecordActivity.java
+++ app/src/main/java/com/thinoo/drcamlink2/activities/LaunchVrecordActivity.java
@@ -1,4 +1,16 @@
 package com.thinoo.drcamlink2.activities;
 
-public class LaunchVrecordActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class LaunchVrecordActivity extends Activity {
+    private final String TAG = LaunchVrecordActivity.class.getSimpleName();
+
+    private static final int VREC_REQUEST = 2100;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
