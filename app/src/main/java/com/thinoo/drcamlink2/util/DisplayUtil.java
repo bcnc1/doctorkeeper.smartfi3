13a187668c692f22a1e430f8304fd3f623cb742c
kimcy <kim.cy@outlook.com>
2019-10-30 16:29:12 +0900

    코드 추가

---
+++ app/src/main/java/com/thinoo/drcamlink2/util/DisplayUtil.java
@@ -0,0 +1,47 @@
+package com.thinoo.drcamlink2.util;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.text.TextUtils;
+import android.util.TypedValue;
+
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class DisplayUtil {
+
+    public static final float dpToPx(Context c, float dp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, c.getResources().getDisplayMetrics());
+    }
+
+    public static boolean saveBitmap(Bitmap bitmap, String destination, boolean recyle){
+        if(bitmap == null || TextUtils.isEmpty(destination)){
+            return false;
+        }
+
+        BufferedOutputStream bos = null;
+        try{
+            FileOutputStream fos = new FileOutputStream(destination);
+            bos = new BufferedOutputStream(fos);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 80, bos);
+            return true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false ;
+        }finally {
+            if (bos != null) {
+                try {
+                    bos.flush();
+                    bos.close();
+                } catch (IOException e) {
+                }
+            }
+            if (recyle) {
+                bitmap.recycle();
+            }
+        }
+    }
+
+}
