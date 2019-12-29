2d21b17f7aa5a3068cc1a38945b057969ffc89bb
kimcy <kim.cy@outlook.com>
2019-12-29 20:06:30 +0900

    workmanager build error오류로 inteentservice 활용하기로 결정

--- app/src/main/java/com/thinoo/drcamlink2/worker/ThumbNailWorker.java
+++ app/src/main/java/com/thinoo/drcamlink2/worker/ThumbNailWorker.java
@@ -2,22 +2,22 @@ package com.thinoo.drcamlink2.worker;
 
 import android.content.Context;
 
-import androidx.annotation.NonNull;
-import androidx.work.Worker;
-import androidx.work.WorkerParameters;
+//import androidx.annotation.NonNull;
+//import androidx.work.Worker;
+//import androidx.work.WorkerParameters;
 
-public class ThumbNailWorker extends Worker {
+public class ThumbNailWorker /*extends Worker*/ {
 
-    private static final String TAG = ThumbNailWorker.class.getSimpleName();
-
-    public ThumbNailWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
-        super(context, workerParams);
-    }
-
-    @NonNull
-    @Override
-    public Result doWork() {
-
-        return null;
-    }
+//    private static final String TAG = ThumbNailWorker.class.getSimpleName();
+//
+//    public ThumbNailWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+//        super(context, workerParams);
+//    }
+//
+//    @NonNull
+//    @Override
+//    public Result doWork() {
+//
+//        return null;
+//    }
 }
--- app/src/main/java/com/thinoo/drcamlink2/worker/WorkerUtils.java
+++ app/src/main/java/com/thinoo/drcamlink2/worker/WorkerUtils.java
@@ -31,10 +31,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.UUID;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.WorkerThread;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
+//import androidx.annotation.NonNull;
+//import androidx.annotation.WorkerThread;
+//import androidx.core.app.NotificationCompat;
+//import androidx.core.app.NotificationManagerCompat;
 
 import static com.thinoo.drcamlink2.Constants.DELAY_TIME_MILLIS;
 
@@ -54,35 +54,35 @@ final class WorkerUtils {
     static void makeStatusNotification(String message, Context context) {
 
         // Make a channel if necessary
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            // Create the NotificationChannel, but only on API 26+ because
-            // the NotificationChannel class is new and not in the support library
-            CharSequence name = Constants.VERBOSE_NOTIFICATION_CHANNEL_NAME;
-            String description = Constants.VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION;
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-            NotificationChannel channel =
-                    new NotificationChannel(Constants.CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-
-            // Add the channel
-            NotificationManager notificationManager =
-                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-            if (notificationManager != null) {
-                notificationManager.createNotificationChannel(channel);
-            }
-        }
-
-        // Create the notification
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentTitle(Constants.NOTIFICATION_TITLE)
-                .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setVibrate(new long[0]);
-
-        // Show the notification
-        NotificationManagerCompat.from(context).notify(Constants.NOTIFICATION_ID, builder.build());
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            // Create the NotificationChannel, but only on API 26+ because
+//            // the NotificationChannel class is new and not in the support library
+//            CharSequence name = Constants.VERBOSE_NOTIFICATION_CHANNEL_NAME;
+//            String description = Constants.VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION;
+//            int importance = NotificationManager.IMPORTANCE_HIGH;
+//            NotificationChannel channel =
+//                    new NotificationChannel(Constants.CHANNEL_ID, name, importance);
+//            channel.setDescription(description);
+//
+//            // Add the channel
+//            NotificationManager notificationManager =
+//                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+//
+//            if (notificationManager != null) {
+//                notificationManager.createNotificationChannel(channel);
+//            }
+//        }
+//
+//        // Create the notification
+//        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+//                .setSmallIcon(R.drawable.ic_launcher_foreground)
+//                .setContentTitle(Constants.NOTIFICATION_TITLE)
+//                .setContentText(message)
+//                .setPriority(NotificationCompat.PRIORITY_HIGH)
+//                .setVibrate(new long[0]);
+//
+//        // Show the notification
+//        NotificationManagerCompat.from(context).notify(Constants.NOTIFICATION_ID, builder.build());
     }
 
     /**
